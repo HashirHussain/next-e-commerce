58f459857fd310b07ce1ce18e05e76c41624d1f2
hashirhussain <hashir.hussain111@gmail.com>
2022-10-22 19:44:53 +0530

    Product list from static data

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
 import Layout from "../components/Layout";
+import ProductItem from "../components/ProductItem";
+import data from "../utils/data";
 
 export default function Home() {
-  return <Layout title="home page">Home Page</Layout>;
+  return (
+    <Layout title="home page">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => {
+          return (
+            <ProductItem product={product} key={product.slug}></ProductItem>
+          );
+        })}
+      </div>
+    </Layout>
+  );
 }
