6a6b76c06913d93c7017e7847d5b26dc679b2489
hashirhussain <hashir.hussain111@gmail.com>
2022-10-24 12:50:49 +0530

    Update quantity

--- pages/cart.js
+++ pages/cart.js
@@ -20,6 +20,14 @@ export default function CartScreen() {
     });
   };
 
+  const updateCartHandler = (item, qty) => {
+    const quantity = Number(qty);
+    dispatch({
+      type: "CART_ADD_ITEM",
+      payload: { ...item, quantity },
+    });
+  };
+
   return (
     <Layout title="Shopping Cart">
       <h1 className="mb-4 text-xl">Shopping Cart</h1>
@@ -57,7 +65,12 @@ export default function CartScreen() {
                       </Link>
                     </td>
                     <td className="p-5 text-right">
-                      <select value={item.quantity}>
+                      <select
+                        value={item.quantity}
+                        onChange={(e) =>
+                          updateCartHandler(item, e.target.value)
+                        }
+                      >
                         {[...Array(item.countInStock).keys()].map((x) => (
                           <option key={x + 1} value={x + 1}>
                             {x + 1}
