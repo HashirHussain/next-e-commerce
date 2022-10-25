c241137fe38e8abcd01bb8021419877d9992321b
hashirhussain <hashir.hussain111@gmail.com>
2022-10-25 19:25:25 +0530

    Develop login functionality

--- pages/login.js
+++ pages/login.js
@@ -1,17 +1,43 @@
+import { signIn, useSession } from "next-auth/react";
 import Link from "next/link";
-import React from "react";
-import Layout from "../components/Layout";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import Layout from "../components/Layout";
+import { getError } from "../utils/error";
 
 export default function LoginScreen() {
+  const { data: session } = useSession();
+  const router = useRouter();
+  const { redirect } = router.query;
+
+  useEffect(() => {
+    if (session?.user) {
+      router.push(redirect || "/");
+    }
+  }, [router, session, redirect]);
+
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm();
 
-  const submitHandler = ({ email, password }) => {};
+  const submitHandler = async ({ email, password }) => {
+    try {
+      const result = await signIn("credentials", {
+        redirect: false,
+        email,
+        password,
+      });
+      if (result.error) {
+        toast.error(result.error);
+      }
+    } catch (error) {
+      toast.error(getError(error));
+    }
+  };
 
   return (
     <Layout title={"login"}>
